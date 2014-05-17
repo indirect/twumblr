6aded635cfb5dc10cdf467929f55c8b9bab2ff68
Andre Arko <andre@arko.net>
2014-05-17 21:55:47 +0100

    sinatra app to turn cloudmailin into posts

---
+++ lib/web.rb
@@ -0,0 +1,13 @@
+require 'sinatra/base'
+require File.expand_path('../twumblr', __FILE__)
+
+class Web < Sinatra::Base
+  set :server, 'puma'
+
+  post "/post" do
+    return 404 unless params["token"] == "EjqIR3T8FWOiGUy9ujFkbfq"
+    Twumblr.new(params["html"]).post
+  end
+end
+
+Web.run! if $0 == __FILE__
