b0c0beb313c43fc508b34a5344c20d248d304a97
Andre Arko <andre@arko.net>
2014-05-23 11:55:18 +0200

    track posted posts in redis for a week

--- lib/web.rb
+++ lib/web.rb
@@ -11,8 +11,32 @@ class Web < Sinatra::Base
 
   post "/post" do
     return 404 unless params["token"] == "EjqIR3T8FWOiGUy9ujFkbfq"
+    return 200 if posted?(params["html"])
     post = Twumblr.new(params["html"]).post
+    mark_posted(params["html"])
+    return post_url(post)
+  end
+
+  def posted?(html)
+    redis.get(sha(html))
+  end
+
+  def mark_posted(html)
+    one_week = 60 * 60 * 24 * 7
+    redis.set(sha(html), "posted", ex: one_week)
+  end
+
+  def sha(html)
+    Digest::SHA256.hexdigest(html)
+  end
+
+  def post_url(post)
     File.join(ENV['TUMBLR_BLOG_URL'], "post", post["id"])
+  end
+
+  def redis
+    require 'redis'
+    @redis ||= Redis.new
   end
 end
 
