12875d147af2a0be430b76058e271369871e05bf
Andre Arko <andre@arko.net>
2023-10-10 22:19:19 -0400

    use vx api to capture tweets again

--- lib/twumblr.rb
+++ lib/twumblr.rb
@@ -30,12 +30,12 @@ class Twumblr
     )
   end
 
-  def photo_data_from(media)
-    media.map do |m|
-      res = Faraday.get(m.media_url)
+  def photo_data_from(urls)
+    urls.map do |u|
+      res = Faraday.get(u)
       io = StringIO.new(res.body)
       type = res.headers["content-type"]
-      filename = m.media_uri.path.split("/").last
+      filename = u.split("/").last
       Faraday::UploadIO.new(io, type, filename)
     end
   end
@@ -84,7 +84,7 @@ class Twumblr
     end
 
     def photos
-      skeet.locate("*/section[@id=images]/img")
+      skeet.locate("*/section[@id=images]/img").map(&:src)
     end
 
     def type
@@ -98,24 +98,12 @@ class Twumblr
     def data
       case type
       when :photo
-        {caption: "#{text} — #{source}", data: photo_data, link: url}
+        {caption: "#{text} — #{source}", data: photo_data_from(photos), link: url}
       when :quote
         {quote: text, source: source, format: "markdown"}
       else
         raise "unimplemented type #{type}!"
       end
-    end
-
-    def photo_data
-      photos.map { |m| create_faraday_upload(m.src) }
-    end
-
-    def create_faraday_upload(url)
-      res = Faraday.get(url)
-      io = StringIO.new(res.body)
-      type = res.headers["content-type"]
-      filename = url.split("/").last
-      Faraday::UploadIO.new(io, type, filename)
     end
 
     def to_tumblr(tumblr)
@@ -181,44 +169,85 @@ class Twumblr
 
   TweetInfo = Struct.new(:tweet) do
     def self.from_url(url)
-      tweet_id = url.match(%r|twitter.com/\w*/status/(\d*)|){|m| m[1] }
+      tweet_id = url.match(%r{(twitter|twittpr|x).com/\w*/status/(\d*)}){|m| m[2] }
       return nil unless tweet_id
 
-      require 'twitter'
-      @twitter ||= Twitter::REST::Client.new(
-        :consumer_key => ENV["TWITTER_API_KEY"],
-        :consumer_secret => ENV["TWITTER_API_SECRET"]
-      )
-      new(@twitter.status(tweet_id, tweet_mode: "extended"))
+      require "http"
+      info_url = "https://api.vxtwitter.com/i/status/#{tweet_id}"
+      new(HTTP.get(info_url).parse)
+    end
+
+    def quoted_tweet
+      @qt ||= TweetInfo.from_url(tweet["qrtURL"]) if tweet["qrtURL"]
     end
 
     def url
-      "http://twitter.com/#{tweet.user.screen_name}/status/#{tweet.id}"
+      tweet["tweetURL"]
+    end
+
+    def quoted_text
+      return unless quoted_tweet
+
+      "<blockquote><p>\n" \
+      "#{quoted_tweet.source}: #{quoted_tweet.text}\n" \
+      "</p></blockquote>\n\n"
     end
 
     def text
-      tweet.attrs[:full_text].
-        gsub(%r{(?:^| )(?:https?\://\S*|pic\.twitter\.com\S*|t.co\S*)}, '').
-        gsub("\n", "\n<br>")
+      "#{quoted_text}#{tweet["text"].gsub("\n", "\n<br>")}"
+    end
+
+    def attribution
+      "#{tweet["user_name"]} (@#{tweet["user_screen_name"]})"
     end
 
     def source
-      %|<a href="#{url}">@#{tweet.user.screen_name}</a>|
+      %|<a href="#{url}">#{attribution}</a>|
     end
 
     def caption
       [text, source].join(" — ")
     end
 
-    def quote_tweet_body
-      quoted = TweetInfo.new(tweet.quoted_tweet)
-      qphoto = tweet.quoted_tweet.media.first
+    def photos
+      tweet["media_extended"].
+        select{|h| h["type"] == "image" }.
+        map{|h| h["url"] }
+    end
 
-      body = "<blockquote><p>\n"
-      body << "#{quoted.source}: #{quoted.text}\n"
-      body << %|<img src="#{qphoto.media_uri}">\n| if qphoto
-      body << "</p></blockquote>\n\n"
-      body << caption
+    def videos
+      tweet["media_extended"].
+        select{|h| h["type"] == "video" }.
+        map{|h| h["url"] }
+    end
+
+    def type
+      if videos.any?
+        :video
+      elsif photos.any?
+        :photo
+      else
+        :quote
+      end
+    end
+
+    def data
+      case type
+      when :video
+        {caption: "#{text} — #{source}", data: photo_data_from(videos), link: url}
+      when :photo
+        {caption: "#{text} — #{source}", data: photo_data_from(photos), link: url}
+      when :quote
+        {quote: text, source: source, format: "markdown"}
+      else
+        raise "unimplemented type #{type}!"
+      end
+    end
+
+    def to_tumblr(tumblr)
+      puts "#{type}:\n#{data.inspect}"
+      return if ENV["DEBUG"]
+      tumblr.send(type, ENV["TUMBLR_BLOG_URL"], data)
     end
   end
 
@@ -340,7 +369,7 @@ class Twumblr
     return chost.to_tumblr(tumblr) if chost
 
     tweet = TweetInfo.from_url(@text) || TweetInfo.from_url(follow_redirects(@text))
-    return tumbl_post(tweet) if tweet
+    return tweet.to_tumblr(tumblr) if tweet
 
     post = PostInfo.from_url(@text)
     return post.to_tumblr(tumblr) if post
